a176ec80ac270ad0d4563be13c60cc98bc4e48c8
Egor Bocharov <dofe71@gmail.com>
2024-04-17 15:50:10 +0000

    draft work stealing queue

--- core/src/engine/task/task_processor.hpp
+++ core/src/engine/task/task_processor.hpp
@@ -12,7 +12,9 @@
 #include <concurrent/impl/interference_shield.hpp>
 #include <engine/task/task_counter.hpp>
 #include <engine/task/task_processor_config.hpp>
-#include <engine/task/task_queue.hpp>
+#include "engine/task/task_queue.hpp"
+#include "engine/task/work_stealing_queue/task_queue.hpp"
+// #include <engine/task/task_queue.hpp>
 #include <utils/statistics/thread_statistics.hpp>
 
 #include <userver/engine/impl/detached_tasks_sync_block.hpp>
